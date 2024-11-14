38edb92d85a4e41b4133462be411c63986aaba1e
ibrohim-abdurahimov <ibr20040322@gmail.com>
2024-11-14 12:40:37 +0500

    detail page

--- src/pages/detail/Detail.jsx
+++ src/pages/detail/Detail.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { memo, useEffect } from 'react'
 import "./Detail.scss"
 import { useParams } from 'react-router-dom'
 import { useFetch } from '../../hooks/UseFetch'
@@ -9,7 +9,9 @@ import site from "../../assets/site.svg"
 const Detail = () => {
     const {id} = useParams()
     const {data,loading,error} = useFetch(`/products/${id}`)
-    console.log(data);
+    useEffect(()=>{
+        window.scrollTo(0, 0)
+    },[])
     
   return (
     <div>
@@ -17,38 +19,40 @@ const Detail = () => {
             <div className="container">
                 <div className="detail__wrapper">
                     <div className="detail__image">
-                        <img src={data.img} alt="" />
+                        <img src={data?.img} alt="" />
                     </div>
                     <div className="detail__text">
-                        <h4 className='detail__title'>{data.title}</h4>
+                        <h4 className="detail__title">
+                            {data?.title}
+                        </h4>
                         <div className="detail__address">
                             <div className="detail__address_text">
                                 <p className="detail__address_city">Scott</p>
                                 <p className="detail__address_street">Артикул : 7655-18p</p>
                                 <p className="detail__address_payment">В наличии</p>
                             </div>
-                            <div className="detail__addres_img">
+                            <div className="detail__address_img">
                                 <img src={site} alt="" />
                             </div>
                         </div>
                         <div className="detail__price">
-                            <h4>{data.price} USD</h4>
-                            <p>{data.lastprice} USD</p>
+                            <h4>{data?.price} USD</h4>
+                            <p>{data?.lastprice} USD</p>
                         </div>
-                        <p className='detail__desc'>{data.desc}</p>
+                        <p className="detail__desc">{data?.desc}</p>
                         <div className="detail__btn">
                             <div className="detail__number">
                                 <div className="detail__number_dec">
-                                    <HiMiniMinusSmall />
+                                    <HiMiniMinusSmall/>
                                 </div>
                                 <p className="detail__number_num">1</p>
                                 <div className="detail__number_inc">
-                                    <HiMiniPlusSmall />
+                                    <HiMiniPlusSmall/>
                                 </div>
                             </div>
                             <button>В корзину</button>
                             <div className="detail__btn_like">
-                                <FaRegHeart />
+                                <FaRegHeart/>
                             </div>
                         </div>
                     </div>
@@ -59,4 +63,4 @@ const Detail = () => {
   )
 }
 
-export default Detail
+export default memo(Detail) 
